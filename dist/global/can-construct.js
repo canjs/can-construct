73214cae449ed38cae2ffd898293d2b0a1bd0083
David Luecke <daff@neyeon.com>
2016-05-18 16:30:41 -0700

    3.0.0-pre.4

---
+++ dist/global/can-construct.js
@@ -0,0 +1,224 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		modules[moduleName] = module && module.exports ? module.exports : result;
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-construct@3.0.0-pre.3#can-construct*/
+define('can-construct', function (require, exports, module) {
+    'use strict';
+    var assign = require('can-util/js/assign/assign');
+    var deepAssign = require('can-util/js/deep-extend/deep-extend');
+    var dev = require('can-util/js/dev/dev');
+    var makeArray = require('can-util/js/make-array/make-array');
+    var types = require('can-util/js/types/types');
+    var namespace = require('can-util/namespace');
+    var initializing = 0;
+    var Construct = function () {
+        if (arguments.length) {
+            return Construct.extend.apply(Construct, arguments);
+        }
+    };
+    var canGetDescriptor;
+    try {
+        Object.getOwnPropertyDescriptor({});
+        canGetDescriptor = true;
+    } catch (e) {
+        canGetDescriptor = false;
+    }
+    var getDescriptor = function (newProps, name) {
+            var descriptor = Object.getOwnPropertyDescriptor(newProps, name);
+            if (descriptor && (descriptor.get || descriptor.set)) {
+                return descriptor;
+            }
+            return null;
+        }, inheritGetterSetter = function (newProps, oldProps, addTo) {
+            addTo = addTo || newProps;
+            var descriptor;
+            for (var name in newProps) {
+                if (descriptor = getDescriptor(newProps, name)) {
+                    this._defineProperty(addTo, oldProps, name, descriptor);
+                } else {
+                    Construct._overwrite(addTo, oldProps, name, newProps[name]);
+                }
+            }
+        }, simpleInherit = function (newProps, oldProps, addTo) {
+            addTo = addTo || newProps;
+            for (var name in newProps) {
+                Construct._overwrite(addTo, oldProps, name, newProps[name]);
+            }
+        };
+    assign(Construct, {
+        constructorExtends: true,
+        newInstance: function () {
+            var inst = this.instance(), args;
+            if (inst.setup) {
+                Object.defineProperty(inst, '__inSetup', {
+                    configurable: true,
+                    enumerable: false,
+                    value: true,
+                    writable: true
+                });
+                args = inst.setup.apply(inst, arguments);
+                inst.__inSetup = false;
+            }
+            if (inst.init) {
+                inst.init.apply(inst, args || arguments);
+            }
+            return inst;
+        },
+        _inherit: canGetDescriptor ? inheritGetterSetter : simpleInherit,
+        _defineProperty: function (what, oldProps, propName, descriptor) {
+            Object.defineProperty(what, propName, descriptor);
+        },
+        _overwrite: function (what, oldProps, propName, val) {
+            what[propName] = val;
+        },
+        setup: function (base) {
+            this.defaults = deepAssign(true, {}, base.defaults, this.defaults);
+        },
+        instance: function () {
+            initializing = 1;
+            var inst = new this();
+            initializing = 0;
+            return inst;
+        },
+        extend: function (name, staticProperties, instanceProperties) {
+            var shortName = name, klass = staticProperties, proto = instanceProperties;
+            if (typeof shortName !== 'string') {
+                proto = klass;
+                klass = shortName;
+                shortName = null;
+            }
+            if (!proto) {
+                proto = klass;
+                klass = null;
+            }
+            proto = proto || {};
+            var _super_class = this, _super = this.prototype, Constructor, namespace, prototype;
+            prototype = this.instance();
+            Construct._inherit(proto, _super, prototype);
+            if (shortName) {
+            } else if (klass && klass.shortName) {
+                shortName = klass.shortName;
+            } else if (this.shortName) {
+                shortName = this.shortName;
+            }
+            function init() {
+                if (!initializing) {
+                    return (!this || this.constructor !== Constructor) && arguments.length && Constructor.constructorExtends ? Constructor.extend.apply(Constructor, arguments) : Constructor.newInstance.apply(Constructor, arguments);
+                }
+            }
+            if (typeof constructorName === 'undefined') {
+                Constructor = function () {
+                    return init.apply(this, arguments);
+                };
+            }
+            for (var propName in _super_class) {
+                if (_super_class.hasOwnProperty(propName)) {
+                    Constructor[propName] = _super_class[propName];
+                }
+            }
+            Construct._inherit(klass, _super_class, Constructor);
+            assign(Constructor, {
+                constructor: Constructor,
+                prototype: prototype,
+                namespace: namespace
+            });
+            if (shortName !== undefined) {
+                Constructor.shortName = shortName;
+            }
+            Constructor.prototype.constructor = Constructor;
+            var t = [_super_class].concat(makeArray(arguments)), args = Constructor.setup.apply(Constructor, t);
+            if (Constructor.init) {
+                Constructor.init.apply(Constructor, args || t);
+            }
+            return Constructor;
+        }
+    });
+    Construct.prototype.setup = function () {
+    };
+    Construct.prototype.init = function () {
+    };
+    var oldIsConstructor = types.isConstructor;
+    types.isConstructor = function (obj) {
+        return obj.prototype instanceof Construct || oldIsConstructor.call(null, obj);
+    };
+    module.exports = namespace.Construct = Construct;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
