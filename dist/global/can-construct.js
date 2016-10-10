abd81dc2cf9357a672a61c433c269e0646ae47f9
John Gardner <john@ardea.io>
2016-10-10 15:19:53 -0500

    3.0.0-pre.10

---
+++ dist/global/can-construct.js
@@ -0,0 +1,472 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val){
+		var parts = name.split("."),
+			cur = global,
+			i, part, next;
+		for(i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if(!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod){
+		if(!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, "default": true };
+		for(var p in mod) {
+			if(!esProps[p]) return false;
+		}
+		return true;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if(globalExport && !get(globalExport)) {
+			if(useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({"can-util/namespace":"can"},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-util@3.0.0-pre.67#js/assign/assign*/
+define('can-util/js/assign/assign', function (require, exports, module) {
+    module.exports = function (d, s) {
+        for (var prop in s) {
+            d[prop] = s[prop];
+        }
+        return d;
+    };
+});
+/*can-util@3.0.0-pre.67#js/is-array/is-array*/
+define('can-util/js/is-array/is-array', function (require, exports, module) {
+    module.exports = function (arr) {
+        return Array.isArray(arr);
+    };
+});
+/*can-util@3.0.0-pre.67#js/is-function/is-function*/
+define('can-util/js/is-function/is-function', function (require, exports, module) {
+    var isFunction = function () {
+        if (typeof document !== 'undefined' && typeof document.getElementsByTagName('body') === 'function') {
+            return function (value) {
+                return Object.prototype.toString.call(value) === '[object Function]';
+            };
+        }
+        return function (value) {
+            return typeof value === 'function';
+        };
+    }();
+    module.exports = isFunction;
+});
+/*can-util@3.0.0-pre.67#js/is-plain-object/is-plain-object*/
+define('can-util/js/is-plain-object/is-plain-object', function (require, exports, module) {
+    var core_hasOwn = Object.prototype.hasOwnProperty;
+    function isWindow(obj) {
+        return obj !== null && obj == obj.window;
+    }
+    function isPlainObject(obj) {
+        if (!obj || typeof obj !== 'object' || obj.nodeType || isWindow(obj)) {
+            return false;
+        }
+        try {
+            if (obj.constructor && !core_hasOwn.call(obj, 'constructor') && !core_hasOwn.call(obj.constructor.prototype, 'isPrototypeOf')) {
+                return false;
+            }
+        } catch (e) {
+            return false;
+        }
+        var key;
+        for (key in obj) {
+        }
+        return key === undefined || core_hasOwn.call(obj, key);
+    }
+    module.exports = isPlainObject;
+});
+/*can-util@3.0.0-pre.67#js/deep-assign/deep-assign*/
+define('can-util/js/deep-assign/deep-assign', function (require, exports, module) {
+    var isArray = require('can-util/js/is-array/is-array');
+    var isFunction = require('can-util/js/is-function/is-function');
+    var isPlainObject = require('can-util/js/is-plain-object/is-plain-object');
+    function deepAssign() {
+        var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {}, i = 1, length = arguments.length;
+        if (typeof target !== 'object' && !isFunction(target)) {
+            target = {};
+        }
+        if (length === i) {
+            target = this;
+            --i;
+        }
+        for (; i < length; i++) {
+            if ((options = arguments[i]) != null) {
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
+                    if (target === copy) {
+                        continue;
+                    }
+                    if (copy && (isPlainObject(copy) || (copyIsArray = isArray(copy)))) {
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            clone = src && isArray(src) ? src : [];
+                        } else {
+                            clone = src && isPlainObject(src) ? src : {};
+                        }
+                        target[name] = deepAssign(clone, copy);
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
+                    }
+                }
+            }
+        }
+        return target;
+    }
+    module.exports = deepAssign;
+});
+/*can-util@3.0.0-pre.67#js/dev/dev*/
+define('can-util/js/dev/dev', function (require, exports, module) {
+});
+/*can-util@3.0.0-pre.67#js/is-array-like/is-array-like*/
+define('can-util/js/is-array-like/is-array-like', function (require, exports, module) {
+    function isArrayLike(obj) {
+        var type = typeof obj;
+        if (type === 'string') {
+            return true;
+        }
+        var length = obj && type !== 'boolean' && typeof obj !== 'number' && 'length' in obj && obj.length;
+        return typeof arr !== 'function' && (length === 0 || typeof length === 'number' && length > 0 && length - 1 in obj);
+    }
+    module.exports = isArrayLike;
+});
+/*can-util@3.0.0-pre.67#js/is-promise/is-promise*/
+define('can-util/js/is-promise/is-promise', function (require, exports, module) {
+    module.exports = function (obj) {
+        return obj instanceof Promise || Object.prototype.toString.call(obj) === '[object Promise]';
+    };
+});
+/*can-util@3.0.0-pre.67#js/types/types*/
+define('can-util/js/types/types', function (require, exports, module) {
+    var isPromise = require('can-util/js/is-promise/is-promise');
+    var types = {
+        isMapLike: function () {
+            return false;
+        },
+        isListLike: function () {
+            return false;
+        },
+        isPromise: function (obj) {
+            return isPromise(obj);
+        },
+        isConstructor: function (func) {
+            if (typeof func !== 'function') {
+                return false;
+            }
+            for (var prop in func.prototype) {
+                return true;
+            }
+            return false;
+        },
+        isCallableForValue: function (obj) {
+            return typeof obj === 'function' && !types.isConstructor(obj);
+        },
+        isCompute: function (obj) {
+            return obj && obj.isComputed;
+        },
+        iterator: typeof Symbol === 'function' && Symbol.iterator || '@@iterator',
+        DefaultMap: null,
+        DefaultList: null,
+        queueTask: function (task) {
+            var args = task[2] || [];
+            task[0].apply(task[1], args);
+        },
+        wrapElement: function (element) {
+            return element;
+        },
+        unwrapElement: function (element) {
+            return element;
+        }
+    };
+    module.exports = types;
+});
+/*can-util@3.0.0-pre.67#js/is-iterable/is-iterable*/
+define('can-util/js/is-iterable/is-iterable', function (require, exports, module) {
+    var types = require('can-util/js/types/types');
+    module.exports = function (obj) {
+        return obj && !!obj[types.iterator];
+    };
+});
+/*can-util@3.0.0-pre.67#js/each/each*/
+define('can-util/js/each/each', function (require, exports, module) {
+    var isArrayLike = require('can-util/js/is-array-like/is-array-like');
+    var has = Object.prototype.hasOwnProperty;
+    var isIterable = require('can-util/js/is-iterable/is-iterable');
+    var types = require('can-util/js/types/types');
+    function each(elements, callback, context) {
+        var i = 0, key, len, item;
+        if (elements) {
+            if (isArrayLike(elements)) {
+                for (len = elements.length; i < len; i++) {
+                    item = elements[i];
+                    if (callback.call(context || item, item, i, elements) === false) {
+                        break;
+                    }
+                }
+            } else if (isIterable(elements)) {
+                var iter = elements[types.iterator]();
+                var res, value;
+                while (!(res = iter.next()).done) {
+                    value = res.value;
+                    callback.call(context || elements, Array.isArray(value) ? value[1] : value, value[0]);
+                }
+            } else if (typeof elements === 'object') {
+                for (key in elements) {
+                    if (has.call(elements, key) && callback.call(context || elements[key], elements[key], key, elements) === false) {
+                        break;
+                    }
+                }
+            }
+        }
+        return elements;
+    }
+    module.exports = each;
+});
+/*can-util@3.0.0-pre.67#js/make-array/make-array*/
+define('can-util/js/make-array/make-array', function (require, exports, module) {
+    var each = require('can-util/js/each/each');
+    function makeArray(arr) {
+        var ret = [];
+        each(arr, function (a, i) {
+            ret[i] = a;
+        });
+        return ret;
+    }
+    module.exports = makeArray;
+});
+/*can-util@3.0.0-pre.67#namespace*/
+define('can-util/namespace', function (require, exports, module) {
+    module.exports = {};
+});
+/*can-construct@3.0.0-pre.9#can-construct*/
+define('can-construct', function (require, exports, module) {
+    'use strict';
+    var assign = require('can-util/js/assign/assign');
+    var deepAssign = require('can-util/js/deep-assign/deep-assign');
+    var dev = require('can-util/js/dev/dev');
+    var makeArray = require('can-util/js/make-array/make-array');
+    var types = require('can-util/js/types/types');
+    var namespace = require('can-util/namespace');
+    var initializing = 0;
+    var Construct = function () {
+        if (arguments.length) {
+            return Construct.extend.apply(Construct, arguments);
+        }
+    };
+    var canGetDescriptor;
+    try {
+        Object.getOwnPropertyDescriptor({});
+        canGetDescriptor = true;
+    } catch (e) {
+        canGetDescriptor = false;
+    }
+    var getDescriptor = function (newProps, name) {
+            var descriptor = Object.getOwnPropertyDescriptor(newProps, name);
+            if (descriptor && (descriptor.get || descriptor.set)) {
+                return descriptor;
+            }
+            return null;
+        }, inheritGetterSetter = function (newProps, oldProps, addTo) {
+            addTo = addTo || newProps;
+            var descriptor;
+            for (var name in newProps) {
+                if (descriptor = getDescriptor(newProps, name)) {
+                    this._defineProperty(addTo, oldProps, name, descriptor);
+                } else {
+                    Construct._overwrite(addTo, oldProps, name, newProps[name]);
+                }
+            }
+        }, simpleInherit = function (newProps, oldProps, addTo) {
+            addTo = addTo || newProps;
+            for (var name in newProps) {
+                Construct._overwrite(addTo, oldProps, name, newProps[name]);
+            }
+        };
+    assign(Construct, {
+        constructorExtends: true,
+        newInstance: function () {
+            var inst = this.instance(), args;
+            if (inst.setup) {
+                Object.defineProperty(inst, '__inSetup', {
+                    configurable: true,
+                    enumerable: false,
+                    value: true,
+                    writable: true
+                });
+                args = inst.setup.apply(inst, arguments);
+                inst.__inSetup = false;
+            }
+            if (inst.init) {
+                inst.init.apply(inst, args || arguments);
+            }
+            return inst;
+        },
+        _inherit: canGetDescriptor ? inheritGetterSetter : simpleInherit,
+        _defineProperty: function (what, oldProps, propName, descriptor) {
+            Object.defineProperty(what, propName, descriptor);
+        },
+        _overwrite: function (what, oldProps, propName, val) {
+            what[propName] = val;
+        },
+        setup: function (base) {
+            this.defaults = deepAssign(true, {}, base.defaults, this.defaults);
+        },
+        instance: function () {
+            initializing = 1;
+            var inst = new this();
+            initializing = 0;
+            return inst;
+        },
+        extend: function (name, staticProperties, instanceProperties) {
+            var shortName = name, klass = staticProperties, proto = instanceProperties;
+            if (typeof shortName !== 'string') {
+                proto = klass;
+                klass = shortName;
+                shortName = null;
+            }
+            if (!proto) {
+                proto = klass;
+                klass = null;
+            }
+            proto = proto || {};
+            var _super_class = this, _super = this.prototype, Constructor, prototype;
+            prototype = this.instance();
+            Construct._inherit(proto, _super, prototype);
+            if (shortName) {
+            } else if (klass && klass.shortName) {
+                shortName = klass.shortName;
+            } else if (this.shortName) {
+                shortName = this.shortName;
+            }
+            function init() {
+                if (!initializing) {
+                    return (!this || this.constructor !== Constructor) && arguments.length && Constructor.constructorExtends ? Constructor.extend.apply(Constructor, arguments) : Constructor.newInstance.apply(Constructor, arguments);
+                }
+            }
+            if (typeof constructorName === 'undefined') {
+                Constructor = function () {
+                    return init.apply(this, arguments);
+                };
+            }
+            for (var propName in _super_class) {
+                if (_super_class.hasOwnProperty(propName)) {
+                    Constructor[propName] = _super_class[propName];
+                }
+            }
+            Construct._inherit(klass, _super_class, Constructor);
+            assign(Constructor, {
+                constructor: Constructor,
+                prototype: prototype
+            });
+            if (shortName !== undefined) {
+                Constructor.shortName = shortName;
+            }
+            Constructor.prototype.constructor = Constructor;
+            var t = [_super_class].concat(makeArray(arguments)), args = Constructor.setup.apply(Constructor, t);
+            if (Constructor.init) {
+                Constructor.init.apply(Constructor, args || t);
+            }
+            return Constructor;
+        }
+    });
+    Construct.prototype.setup = function () {
+    };
+    Construct.prototype.init = function () {
+    };
+    var oldIsConstructor = types.isConstructor;
+    types.isConstructor = function (obj) {
+        return obj.prototype instanceof Construct || oldIsConstructor.call(null, obj);
+    };
+    module.exports = namespace.Construct = Construct;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
